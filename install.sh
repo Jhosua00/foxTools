302485beb57f672d82ebd67bca97f7a27f15ee62
Mr.Lucifer <47558106+Jhosua00@users.noreply.github.com>
2019-05-15 07:45:47 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+pkg update && pkg upgrade -y
+pkg install python2 -y
+pkg install php -y
+pkg install nano
+pkg install git -y
+pkg install curl -y
+pkg install ruby -y
+pkg install mtools
+pkg install gem
+pkg install toilet
+pkg install figlet 
+pip2 install mechanize
+pip install requests
+pip2 install lolcat
+pkg install openssh
+pip2 install bs4
+pip2 install urllib3 chardet certifi idna requests
+pip2 install -r requirements.txt
