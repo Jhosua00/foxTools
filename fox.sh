074b0179026ddca1245627d5703f0c28d4be0b83
Mr.Lucifer <47558106+Jhosua00@users.noreply.github.com>
2019-05-17 19:35:27 +0700

    Add files via upload

---
+++ fox.sh
@@ -0,0 +1,47 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+
+echo $me"MAAF TOOLS INI TIDAK DAPAT DI INSTALL...."
+echo $me"UNTUK MENGINSTALL TOOLS INI DI KENAKAN TARIF 10k"
+echo $i"JIKA ANDA MAU PREMIUM CUKUP BAYAR 20k"
+echo $i"JIKA PREMIUM ANDA AKAN DAPAT UPDATE TOOLS PERMANET"
+echo $i"JIKA TRIAL MAU TRIAL ANDA AKAN DAPAT UPDATE TOOLS HANYA 2 MINGGU"
+echo $i"UPDATE SCRIPT DILAKUKAN SETIAP 1 MINGGU SEKALI"
+echo "KENAPA BAYAR? "
+echo $cy"KARENA NULIS NYA CAPEK BRO"
+echo $cy"APAKAH ANDA BERMINAT MEMBELI TOOLSNYA?"
+echo $i"[1] YA"
+echo $me"[2]TIDAK"
+
+read -p "JAWAB :" pil; 
+
+if [ $pil = 1 ]
+then
+clear
+echo $i"TERIMAH KASIH TELAH MEMBELI TOOLS KAMI"
+echo $i"UNTUK PEMBAYARAN SILAHKAN HUB :"
+echo $cy"============================"
+echo $cy"WA   : 085275684845"
+echo $cy"FB   : Jhosua Saut Maruli"
+echo $cy"============================"
+echo $i"SILAHKAN DI HUBUNGI"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo $me"OWH....:-("
+echo $me"KAMI MENERIMA KEPUTUSAN ANDA :-)"
+echo $me"MUNGKIN LAIN WAKTU KAMU MAU MEMBELI TOOLS SAYA"
+echo $i"KAMI MENERIMA KEPUTUSAN ANDA"
+echo $i"KAMI AKAN MENINGKATKAN TOOLS KAMI :-)"
+echo $i"TERIMAH KASIH :-)"
+fi
