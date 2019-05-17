24f651fcfae35fef1a499b5af1fb85985e9cf89c
Mr.Lucifer <47558106+Jhosua00@users.noreply.github.com>
2019-05-17 19:38:58 +0700

    Add files via upload

---
+++ fox.sh
@@ -0,0 +1,46 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+sleep 1
+echo $me"MAAF TOOLS INI TIDAK DAPAT DI INSTALL"
+sleep 1
+echo $me"UNTUK INSTALL BACA SYARAT & KETENTUAN"
+sleep 1
+echo $cy"==========================================="
+echo $i"[1]"$pu"SYARAT DAN KETENTUAN"
+echo $i"[2]"$pu"EXIT"
+echo
+read -p "JAWAB :" pil;
+
+if [ $pil = 1 ]
+then
+clear
+echo "SYARAT DAN KETENTUAN"
+echo "SCRIPT DI UPDATE 1x SEMINGGU"
+echo "PREMIUM : 20k"
+echo "TRIAL   : 10k"
+echo "APA BEDANYA?"
+echo "PREMIUM = ANDA AKAN MENERIMA UPDATE PERMANET"
+echo "TRIAL   = ANDA DAPAT UPDATE HANYA 2 MINGGU"
+echo
+echo "SETIAP ADA BELI SCRIPT PASSWORD AKAN DI GANTI!!"
+echo "TENANG AJH ANDA TETAP MASIH BISA MENGKASESNYA"
+echo 
+echo "JIKA BERMINAT SILAHKAN PM WA :"
+echo "WA : 085275684945"
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo "OKE"
+echo "KAMI MENERIMA KEPUTUSAN ANDA"
+fi
